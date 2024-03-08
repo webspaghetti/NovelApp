a94ae5447be0d116b38f0c371aa3565a04e64164
webspaghetti <ondrakohondra@email.cz>
2024-03-08 15:54:29 +0100

    Add navbar to layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/app/components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <NavBar />
+      {children}</body>
     </html>
   );
 }
