dae92f4cd204a7a3d81baca906d1cf2a6d31e4fa
webspaghetti <ondrakohondra@email.cz>
2024-03-08 16:12:54 +0100

    Add logo to NavBar and div to have full width effect

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
+import Logo from './logo.png';
 
 function NavBar() {
     return (
-        <nav>
-            <h1>NovelApp</h1>
-            <Link href={"/"}>Homepage</Link>
-            <Link href={"/about"}>About</Link>
-        </nav>
+        <div className={"w-full bg-navbar"}>
+            <nav>
+                <Image src={Logo} alt={"NovelApp logo"} width={70} quality={100} placeholder={'blur'}/>
+                <h1>NovelApp</h1>
+                <Link href={"/"}>Homepage</Link>
+                <Link href={"/about"}>About</Link>
+            </nav>
+        </div>
     );
 }
 
