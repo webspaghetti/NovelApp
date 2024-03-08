2554b6c9f88659f3ebca7402bf9057f6e6b2d9dc
webspaghetti <ondrakohondra@email.cz>
2024-03-08 20:04:26 +0100

    Change h1 gap

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
     return (
         <div className={"w-full bg-navbar"}>
             <nav>
-                <Link href={"/"}><div className={"flex items-center gap-5"}>
+                <Link href={"/"}><div className={"flex items-center gap-7"}>
                     <Image src={Logo} alt={"NovelApp logo"} width={70} quality={100}/>
                 <h1 className={"text-3xl h1_outline"}>NovelApp</h1>
                 </div></Link>
