9642826eb47be39daf31974cbc2c86d7b65f27d8
webspaghetti <ondrakohondra@email.cz>
2024-03-08 22:14:55 +0100

    Add button

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
     <main>
-      <h2>Hello World</h2>
+
+        <div className={"flex justify-center lg:justify-start w-full mb-5"}> {/*Add novel button*/}
+            <button>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+                Add novel
+            </button>
+        </div>
+
+
+
     </main>
   );
 }
