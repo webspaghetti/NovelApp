d2875fa9f518538c833f980b0af238708f3c3337
webspaghetti <ondrakohondra@email.cz>
2024-03-08 16:12:18 +0100

    Add colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors:{
+        primary: '#5e42cf',
+        secondary: '#FAFAFA',
+        navbar: '#121314'
+      }
     },
   },
   plugins: [],
