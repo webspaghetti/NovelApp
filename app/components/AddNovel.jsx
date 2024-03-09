f9a61a88427df5b66cd8301924e9e510e79aecba
webspaghetti <ondrakohondra@email.cz>
2024-03-09 16:23:38 +0100

    Move the add novel logic to AddNovel.jsx

---
+++ app/components/AddNovel.jsx
@@ -0,0 +1,22 @@
+"use client"
+import {useState} from "react";
+import PopupForm from "@/app/components/PopupForm";
+
+function AddNovel() {
+
+    const [buttonPopup, setButtonPopup] = useState(false);
+
+    return (
+        <>
+            <button onClick={() => setButtonPopup(true)} className={"text-primary"}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+                Add novel
+            </button>
+            <PopupForm trigger={buttonPopup} setTrigger={setButtonPopup} />
+        </>
+    );
+}
+
+export default AddNovel;
