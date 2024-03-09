861a551ab8a20793922a72393ccc86b97d929701
webspaghetti <ondrakohondra@email.cz>
2024-03-09 03:33:44 +0100

    Add custom borderWidth to badges

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
         navbar: '#121314',
         card: '#212121',
         main_background: '#191919' /*#202020 #191919*/
+      },
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '2': '2px',
+        '3': '3px',
+        '4': '4px',
+        '6': '6px',
+        '8': '8px'
       }
     },
   },
