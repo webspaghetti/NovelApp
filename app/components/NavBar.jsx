46f6f1544b75a2909703599d63f2b3e848000089
webspaghetti <ondrakohondra@email.cz>
2024-03-09 14:30:08 +0100

    Change NavBar title color

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar() {
             <nav>
                 <Link href={"/"}><div className={"flex items-center gap-5"}>
                     <Image src={Logo} alt={"NovelApp logo"} width={60} quality={100}/>
-                <h1 className={"text-2xl h1_outline"}>NovelApp</h1>
+                <h1 className={"text-2xl link_outline text-secondary"}>NovelApp</h1>
                 </div></Link>
                 <div className={"flex items-center gap-16"}>
                 <Link href={"/"}>Home</Link>
