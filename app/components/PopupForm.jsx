f63bb72a4fe47905b6b145d3bf88e41bc169b492
webspaghetti <ondrakohondra@email.cz>
2024-03-09 16:23:02 +0100

    Create a PopupComponent

---
+++ app/components/PopupForm.jsx
@@ -0,0 +1,23 @@
+function PopupForm(props) {
+
+
+  return (props.trigger) ? (
+    <div className="fixed top-0 left-0 w-full h-full bg-navbar bg-opacity-60 flex justify-center items-center z-10">
+      <div className="relative p-8 w-full max-w-xl bg-navbar border-primary border-4 rounded-3xl">
+        <button onClick={() => props.setTrigger(false)} className="absolute top-3 right-3 flex-shrink-0 text-sm border-4 py-1 px-2 rounded-lg border-red-700 hover:bg-red-700 text-red-700">Close</button>
+
+          <form className={"bg-main_background p-4 rounded-lg shadow-md w-full"}>
+              <div className="flex items-center border-b-2 border-primary py-2">
+                  <input className="appearance-none bg-transparent border-none w-full text-secondary mr-3 px-2 leading-tight focus:outline-none" type="text" placeholder="Link" aria-label="Link" />
+                  <button className="flex-shrink-0 text-sm border-4 text-primary py-3 px-3 rounded-lg mb-2" type="submit">
+                      Submit
+                  </button>
+              </div>
+          </form>
+
+      </div>
+    </div>
+  ) : "";
+}
+
+export default PopupForm;
