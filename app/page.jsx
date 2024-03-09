85edbf2cb3276c7f164d5919c3dc3332262d2641
webspaghetti <ondrakohondra@email.cz>
2024-03-09 02:18:20 +0100

    Implement displaying of novels from mysql

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,18 @@
 import Image from "next/image";
+import executeQuery from "@/app/database/db";
 
-export default function Home() {
+export default async function Home() {
+
+    const novels = await executeQuery("select * from novel_table;");
+
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${day}.${month}.${year}`;
+    }
+
   return (
     <main>
 
@@ -15,16 +27,19 @@ export default function Home() {
 
         <div className={"grid grid-cols-3 gap-10"}> {/*Cards layout*/}
 
-            <div className={"card"}>
-                <Image src={"/stew.jpg"} alt={"NovelApp logo"} width={1000} height={1000} quality={100} className={"w-full h-72 sm:h48 object-cover"}/>
-                <div className="m-4">
-                    <span className="text-secondary font-bold text-lg">Veg Noodles</span>
-                    <span className="block text-gray-400 text-sm">Recipe by Mario</span>
+            {novels.map((novel) => (
+                <div key={novel.id} className={"card"}>
+                    <Image src={`/thumbnails/${novel.formatted_name}.jpg`} alt={"NovelApp logo"} width={1000} height={1000} quality={100} className={"w-full h-96 sm:h48 object-cover"}/>
+                    <div className="m-4">
+                        <span className="text-secondary font-bold text-lg">{novel.name}</span>
+                        <span className="block text-gray-400 text-sm">Chapters: {novel.chapter_count}</span>
+                        <span className={"block text-gray-400 text-sm"}>Last updated: {formatDate(novel.latest_update)}</span>
+                    </div>
+                    <div className={`badge ${novel.status}`}>
+                        <span>{novel.status}</span>
+                    </div>
                 </div>
-                <div className="badge Complete">
-                    <span>25 mins</span>
-                </div>
-            </div>
+            ))}
 
         </div>
 
