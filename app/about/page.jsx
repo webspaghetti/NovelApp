c166f84841e0be96899df299169d5b7dd36242b9
webspaghetti <ondrakohondra@email.cz>
2024-03-09 16:26:00 +0100

    Changes related to making NavBar sticky

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,7 @@
 function Page() {
     return (
         <main>
-            <div className="card py-5 px-2">
+            <div className="card py-5 px-2 relative top-20">
                 <h1 className={"text-secondary text-3xl link_outline"}>About</h1>
                 <p>Just a small personal project to improve my reading experience on IOS using a PWA.</p>
             </div>
--- app/page.jsx
+++ app/page.jsx
@@ -5,18 +5,11 @@ export default function Home() {
 
   return (
     <main>
-
-        <div className={"flex justify-center lg:justify-start w-full mb-5"}> {/*Add novel button*/}
-            <button>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                </svg>
-                Add novel
-            </button>
+        <div className={"flex justify-center lg:justify-start w-full mb-5 relative top-20"}> {/*Add novel button*/}
             <AddNovel />
         </div>
 
-        <div className={"grid grid-cols-3 gap-10"}> {/*Cards layout*/}
+        <div className={"grid grid-cols-3 gap-10 relative top-20"}> {/*Cards layout*/}
             <LoadNovels />
         </div>
 
