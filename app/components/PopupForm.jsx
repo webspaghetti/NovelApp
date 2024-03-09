92f76549db5de967f6b4f4c83e2ea76f8e0a384d
webspaghetti <ondrakohondra@email.cz>
2024-03-09 17:10:17 +0100

    Stylize PopupForm.jsx close button

--- app/components/PopupForm.jsx
+++ app/components/PopupForm.jsx
@@ -2,8 +2,8 @@ function PopupForm(props) {
 
   return (props.trigger) ? (
     <div className="fixed top-0 left-0 w-full h-full flex bg-navbar bg-opacity-20 justify-center items-center z-10 backdrop-blur-sm">
-      <div className="relative p-8 w-full max-w-xl bg-navbar border-primary border-4 rounded-3xl">
-        <button onClick={() => props.setTrigger(false)} className="absolute top-3 right-3 flex-shrink-0 text-sm border-4 py-1 px-2 rounded-lg border-red-700 hover:bg-red-700 text-red-700">Close</button>
+      <div className="relative p-6 pt-14 w-full max-w-xl bg-navbar border-primary border-4 rounded-3xl">
+        <button onClick={() => props.setTrigger(false)} className="absolute top-3 right-6 flex-shrink-0 text-sm border-4 py-1 px-2 rounded-lg border-red-700 hover:bg-red-700 text-red-700">Close</button>
 
           <form className={"bg-main_background p-4 rounded-lg shadow-md w-full"}>
               <div className="flex items-center border-b-2 border-primary py-2">
