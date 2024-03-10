61318d7123c27203a49df899d175cd4b85177090
webspaghetti <ondrakohondra@email.cz>
2024-03-10 01:41:49 +0100

    Add user progress to novel pages

--- app/[formatted_name]/page.jsx
+++ app/[formatted_name]/page.jsx
@@ -22,23 +22,43 @@ async function getNovel(f_name){
     }
 }
 
+async function getUserInfo(n_id){
+    try {
+        return await executeQuery(`SELECT * FROM user_progress WHERE user_id = 1 AND novel_id = ${n_id}`); // Just me :)
+    } catch (error) {
+        console.error("Oops, an error occurred:", error);
+        return null;
+    }
+}
+
 async function Page({params}) {
-    const novels = await getNovel(params.formatted_name)
+    const novels = await getNovel(params.formatted_name);
     const novel = novels[0]; // Access the first novel in the array
 
+    const users = await getUserInfo(novel.id);
+    const user = users[0];
+
     return (
-        <main className="relative top-20 flex flex-col md:flex-row justify-center items-center gap-8">
-            <div className="w-full md:w-1/3">
-                <Image src={`/thumbnails/${novel.formatted_name}.jpg`} alt={`${novel.name} thumbnail`} width={350} height={350} quality={100} className="select-none rounded-lg shadow-lg" />
-            </div>
-            <div className="flex flex-col justify-center items-start md:w-1/3">
-                <div className={`badge ${novel.status} select-none relative top-20 mb-4`}>
-                    <span>{novel.status}</span>
+        <main className="relative top-20">
+            <div className={'flex flex-col md:flex-row justify-center items-center gap-8 border-b-3 border-b-gray-400 pb-4'}>
+
+                <div className="w-full md:w-1/3">
+                    <Image src={`/thumbnails/${novel.formatted_name}.jpg`} alt={`${novel.name} thumbnail`} width={350} height={350} quality={100} className="select-none rounded-lg shadow-lg" />
                 </div>
-                <h1 className="text-3xl font-bold mb-2">{novel.name}</h1>
-                <p className="text-lg mb-2">Chapters: {novel.chapter_count}</p>
-                <p className="text-lg mb-4">Last updated: <DateFormatter dateString={novel.latest_update} /></p>
-                <button className="bg-primary text-secondary py-2 px-6 rounded-lg shadow-md hover:bg-primary-dark transition-colors duration-300">Start reading</button>
+                <div className="flex flex-col justify-center items-start md:w-1/3">
+                    <div className={`badge ${novel.status} select-none relative top-20 mb-4`}>
+                        <span>{novel.status}</span>
+                    </div>
+                    <h1 className="text-3xl font-bold mb-2">{novel.name}</h1>
+                    <p className="text-lg mb-2">Chapters: {novel.chapter_count}</p>
+                    <p className="text-lg mb-4">Last updated: <DateFormatter dateString={novel.latest_update} /></p>
+                    <button className="bg-primary text-secondary py-2 px-6 rounded-lg shadow-md hover:bg-primary-dark transition-colors duration-300">
+                        {user?.novel_chapter === undefined
+                            ? <Link href={"/"}>Start reading</Link>
+                            : <Link href={"/about"}>Continue reading - Chapter: {user.novel_chapter} </Link>}
+                    </button>
+                </div>
+
             </div>
         </main>
 
