f975175fa63b8466ce52ea45010d593b91e7dab2
webspaghetti <ondrakohondra@email.cz>
2024-03-12 00:17:51 +0100

    Change layout.jsx formatting

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,8 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <NavBar />
-      {children}</body>
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
