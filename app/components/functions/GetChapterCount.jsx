09309d06090518e858baaf01f641832762aa87fb
webspaghetti <ondrakohondra@email.cz>
2024-03-12 21:27:40 +0100

    Get rid of GetNovel.js function and implement GetChapterCount.jsx component instead

---
+++ app/components/functions/GetChapterCount.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import executeQuery from "@/app/database/db";
+
+export default function GetChapterCount({ formattedName }) {
+    const [chapterCount, setChapterCount] = useState(0);
+
+    useEffect(() => {
+        async function fetchChapterCount() {
+            try {
+                const novels = await executeQuery(`SELECT * FROM novel_table WHERE formatted_name = '${formattedName}'`);
+                const novel = novels[0];
+                setChapterCount(novel.chapter_count);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        fetchChapterCount();
+    }, [formattedName]);
+
+    return chapterCount;
+}
