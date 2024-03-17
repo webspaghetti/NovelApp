594a695b155dd4c40d9ea59353e1596d055ee856
webspaghetti <ondrakohondra@email.cz>
2024-03-17 14:32:23 +0100

    Add LoadingPopup.jsx for refreshing novels

---
+++ app/components/LoadingPopup.jsx
@@ -0,0 +1,11 @@
+import CircularProgress from '@mui/material/CircularProgress';
+function LoadingPopup(props) {
+
+    return (props.trigger) ? (
+        <div className="fixed top-0 left-0 w-full h-full flex bg-navbar bg-opacity-50 justify-center items-center z-20 backdrop-blur-sm">
+            <CircularProgress sx={{color: "#5e42cf"}} size={150}/>
+        </div>
+    ) : null;
+}
+
+export default LoadingPopup;
