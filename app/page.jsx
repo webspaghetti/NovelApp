ada229df202439f2c67361ddc61fe3a3ee8caef0
webspaghetti <ondrakohondra@email.cz>
2024-03-17 14:01:53 +0100

    Change front-end to account for FetchNovels.jsx button

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,15 @@
 import LoadNovels from "@/app/components/LoadNovels";
 import AddNovel from "@/app/components/AddNovel";
+import FetchNovels from "@/app/components/FetchNovels";
 
 export default function Home() {
 
   return (
     <main>
 
-        <div className={"flex justify-center lg:justify-start w-full mb-5 relative top-20"}> {/*Add novel button*/}
+        <div className={"flex justify-center lg:justify-between w-full mb-5 relative top-20"}> {/*Add novel button*/}
             <AddNovel />
+            <FetchNovels />
         </div>
 
         <div className={"grid grid-cols-3 gap-10 relative top-20 pb-9"}> {/*Cards layout*/}
