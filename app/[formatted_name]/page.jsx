c5db91ce41dd2f165863d9338f9710c420fc289f
webspaghetti <ondrakohondra@email.cz>
2024-03-17 22:22:29 +0100

    Improve novel page layout

--- app/[formatted_name]/page.jsx
+++ app/[formatted_name]/page.jsx
@@ -1,10 +1,9 @@
 "use client"
 import executeQuery from "@/app/database/db";
-import DateFormatter from "@/app/components/functions/DateFormatter";
+import React, {useEffect, useState} from "react";
+import NovelPage from "@/app/[formatted_name]/NovelPage";
 import ChapterButtons from "@/app/[formatted_name]/ChapterButtons";
-import Link from "next/link";
-import Image from "next/image";
-import { useEffect, useState } from 'react';
+import {notFound} from "next/navigation";
 import CircularProgress from "@mui/material/CircularProgress";
 
 export const dynamicParams = true;
@@ -28,79 +27,48 @@ async function getUserInfo(n_id){
     }
 }
 
-function isValidDate(dateString) {
-    return !isNaN(Date.parse(dateString));
-}
-
 function Page({params}) {
     const [novel, setNovel] = useState(null);
     const [user, setUser] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const novels = await getNovel(params.formatted_name);
-                const novel = novels[0]; // Access the first novel in the array
+                const fetchedNovels = await getNovel(params.formatted_name);
 
-                const users = await getUserInfo(novel.id);
-                const user = users[0];
-
-                setNovel(novel);
-                setUser(user);
-                setIsLoading(false);
+                if (fetchedNovels && fetchedNovels.length > 0) {
+                    const fetchedUser = await getUserInfo(fetchedNovels[0].id);
+                    setUser(fetchedUser[0]);
+                    setNovel(fetchedNovels[0]);
+                }
             } catch (error) {
-                setError(error);
-                setIsLoading(false); // Hide loading indicator even in case of error
+                console.error('Error fetching data:', error);
+            } finally {
+                setIsLoading(false); // Always set loading state to false
             }
         };
 
         fetchData();
-
-        // ... polling logic for updates
     }, [params.formatted_name]);
 
     return (
         <main className="relative top-20">
-            {isLoading || !user ? (
+            {isLoading ? (
                 <div className="relative flex justify-center items-center top-60">
                     <CircularProgress sx={{color: "#5e42cf"}} size={150}/>
                 </div>
-            ) : error ? (
-                <div className="flex justify-center items-center h-full">
-                    <p>Error fetching data: {error.message}</p>
-                </div>
-            ) : (
+            ) : novel ?(
                 <>
-                    <div className={'flex flex-col md:flex-row justify-center items-center gap-8 border-b-3 border-b-gray-400 pb-4'}>
-                        <div className="w-full md:w-1/3">
-                            <Image src={novel.alternative_image_url ? novel.alternative_image_url : novel.image_url} alt={`${novel.name} thumbnail`} width={350} height={466} quality={100} className="select-none rounded-lg shadow-lg" draggable="false" />
-                        </div>
-                        <div className="flex flex-col justify-center items-start md:w-1/3">
-                            <div className={`badge ${novel.status} select-none relative top-20 mb-4`}>
-                                <span>{novel.status}</span>
-                            </div>
-                            <h1 className="text-3xl font-bold mb-2">{novel.name}</h1>
-                            <p className="text-lg mb-2">Chapters: {novel.chapter_count}</p>
-                            <p className={"block text-gray-400 text-sm"}> Last updated: {isValidDate(novel.latest_update) ? (
-                                <DateFormatter dateString={novel.latest_update} />
-                            ) : (
-                                novel.latest_update
-                            )}
-                            </p>
-                            <button className="bg-primary text-secondary my-2 py-2 px-6 rounded-lg shadow-md hover:bg-primary-dark transition-colors duration-300">
-                                {user?.current_chapter === null
-                                    ? <Link href={`/${novel.formatted_name}/1`}>Start reading</Link>
-                                    : <Link href={`/${novel.formatted_name}/${user.current_chapter}`}>Continue reading - Chapter: {user.current_chapter} </Link>}
-                            </button>
-                        </div>
-                    </div>
-                    <ChapterButtons novel={novel} user={user}/>
+                <NovelPage user={user} novel={novel} />
+                <ChapterButtons novel={novel} user={user} />
                 </>
+            ): (
+                notFound()
             )}
         </main>
     );
+
 }
 
 export default Page;
