f612d33f226da3185623446bc9ce2c77c974f98a
webspaghetti <ondrakohondra@email.cz>
2024-03-18 16:51:33 +0100

    Add navbar responsivity

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -23,7 +23,7 @@ function NavBar() {
 
     return (
         <div className={"w-full bg-navbar select-none " + navbarStyles}>
-            <nav>
+            <nav className={"max-md:mx-5"}>
                 <Link href={"/"}><div className={"flex items-center gap-5"}>
                     <Image src={Logo} alt={"NovelApp logo"} width={60} quality={100}/>
                 <h1 className={"text-2xl link_outline text-secondary"}>NovelApp</h1>
