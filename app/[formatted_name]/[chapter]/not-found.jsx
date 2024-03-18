4eac2b59c6441986810abaf56926e67d631f5bf5
webspaghetti <ondrakohondra@email.cz>
2024-03-18 09:43:29 +0100

    Improve chapter page not-found.jsx

--- app/[formatted_name]/[chapter]/not-found.jsx
+++ app/[formatted_name]/[chapter]/not-found.jsx
@@ -1,7 +1,21 @@
 "use client"
+import {useEffect, useState} from "react";
+import {usePathname} from "next/navigation";
+
 function NotFound() {
+    const pathname = usePathname();
+    const [location, setLocation] = useState('');
+
+    useEffect(() => {
+        // Extract the last section of the pathname
+        const novelName = pathname.split('/')[1];
+
+        // Check if the last section is a number (using isNaN)
+        setLocation(novelName);
+    }, [pathname]);
+
     const handleNavigation = () => {
-        window.location.href = `/`; // Navigate to homepage
+        window.location.href = `/${location}`; // Navigate to homepage
     };
 
     return (
@@ -9,7 +23,7 @@ function NotFound() {
             <h2 className={"text-5xl"}>Chapter Not Found</h2>
             <p className={'text-lg'}>We couldn&apos;t find the chapter you were looking for.</p>
             <div className={"flex justify-center my-5"}>
-                <button onClick={handleNavigation}>Go back to the Homepage</button>
+                <button onClick={handleNavigation}>Go back to the Gallery</button>
             </div>
         </main>
     );
