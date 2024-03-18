90ae3cdf26fc03a974a26c9d86584cc0a308b7a9
webspaghetti <ondrakohondra@email.cz>
2024-03-18 09:11:10 +0100

    Add select-none to About page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,7 +2,7 @@ function Page() {
     return (
         <main>
             <div className="card py-5 px-2 relative top-20">
-                <h1 className={"text-secondary text-3xl link_outline"}>About</h1>
+                <h1 className={"text-secondary text-3xl link_outline select-none"}>About</h1>
                 <p>Just a small personal project to improve my reading experience on IOS using a PWA.</p>
             </div>
         </main>
