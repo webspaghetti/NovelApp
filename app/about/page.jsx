8b290cf18ea8e782aa9d8256ecd99a556f3172c4
webspaghetti <ondrakohondra@email.cz>
2024-05-28 19:25:08 +0200

    Improve about page design

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,9 +1,9 @@
 function Page() {
     return (
         <main>
-            <div className="card py-5 px-2 relative top-20">
+            <div className="bg-navbar shadow-sm rounded-xl overflow-hidden border-primary border-4 text-center py-5 px-2 relative top-20">
                 <h1 className={"text-secondary text-3xl link_outline select-none"}>About</h1>
-                <p>Just a small personal project to improve my reading experience on IOS using a PWA.</p>
+                <p className={"my-4 leading-6 text-secondary text-lg"}>Just a small personal project to improve my reading experience on IOS using a PWA.</p>
             </div>
         </main>
     );
