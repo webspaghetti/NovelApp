59c830307d91ed462f155dafff3cfdb1892b65e0
webspaghetti <ondrakohondra@email.cz>
2024-05-28 20:02:24 +0200

    Add novel progress on the home page

--- app/components/LoadNovels.jsx
+++ app/components/LoadNovels.jsx
@@ -9,16 +9,21 @@ async function LoadNovels() {
     }
 
     const novels = await executeQuery("select * from novel_table;");
+    const users = await executeQuery("select * from user_progress where user_id = 1;");
 
     return (
-        novels.map((novel) => (
+        novels.map((novel) => {
 
+            const userProgress = users.find(user => user.novel_id === novel.id);
+            const currentChapter = userProgress && userProgress.current_chapter !== null ? userProgress.current_chapter : 0;
+
+            return (
             <div key={novel.id} className={"card glassy-animation max-md:h-101 max-sm:h-75"}>
                 <Link href={`/${novel.formatted_name}`}>
                     <Image src={novel.alternative_image_url ? novel.alternative_image_url : novel.image_url} alt={`${novel.name} thumbnail`} width={1000} height={1000} quality={100} className={"w-full h-100 md:h-96 max-sm:h-52 object-cover select-none img"} draggable="false"/>
                     <div className="m-4">
                         <div className={"text-secondary font-bold text-lg max-sm:text-base max-w-30 max-sm:max-w-52 truncate max-sm:hidden"}>{novel.name}</div>
-                        <span className={"block text-gray-400 text-sm"}>Chapters: {novel.chapter_count}</span>
+                        <span className={"block text-gray-400 text-sm"}>Progress: {currentChapter} / {novel.chapter_count}</span>
                         <span className={"block text-gray-400 text-sm"}> Last updated: {isValidDate(novel.latest_update) ? (
                             <DateFormatter dateString={novel.latest_update} />
                         ) : (
@@ -32,7 +37,8 @@ async function LoadNovels() {
                 </Link>
             </div>
 
-        ))
+            );
+        })
     );
 }
 
