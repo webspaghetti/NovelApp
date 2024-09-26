6fb5f88a4596d3b981e14895048cf8a56d33b46f
webspaghetti <ondrakohondra@email.cz>
2024-09-26 17:43:35 +0200

    Add scaling animation

--- app/components/AddNovel.jsx
+++ app/components/AddNovel.jsx
@@ -8,7 +8,7 @@ function AddNovel() {
 
     return (
         <>
-            <button onClick={() => setButtonPopup(true)} className={"text-secondary mr-2"}>
+            <button onClick={() => setButtonPopup(true)} className={"text-secondary mr-2 svg-animate-scale"}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg>
