d4e6e00b63c5c7842ea2ceee551d8f43f04aec6f
webspaghetti <ondrakohondra@email.cz>
2024-10-03 22:29:34 +0200

    Add HoverRevealButton.jsx

---
+++ app/components/HoverRevealButton.jsx
@@ -0,0 +1,28 @@
+export default function HoverRevealButton({ onClick, label, animation, shape }){
+    return (
+        <button
+            onClick={onClick}
+            className={`group flex items-center text-secondary md:pl-4 ${animation} relative`}
+            aria-label={label}
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-7 h-7 z-10"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d={shape}
+                />
+            </svg>
+            <div className="absolute left-0 w-7 h-7 bg-transparent group-hover:w-full transition-all duration-300 ease-in-out" />
+            <span className="max-sm:hidden overflow-hidden opacity-0 max-w-0 group-hover:max-w-xs group-hover:opacity-100 transition-all duration-1000 ease-in-out">
+        <span className="pl-2 whitespace-nowrap">{label}</span>
+      </span>
+        </button>
+    );
+}
