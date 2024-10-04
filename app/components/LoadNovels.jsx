7fffd01f7538b3ce4214742d8322f0b7848ec273
webspaghetti <ondrakohondra@email.cz>
2024-10-04 03:39:06 +0200

    Update badge padding along with OnGoing badge custom resize

--- app/components/LoadNovels.jsx
+++ app/components/LoadNovels.jsx
@@ -76,6 +76,8 @@ function LoadNovels({ novels }) {
                         </div>
                         <div className={`badge ${novel.status} max-sm:p-2 absolute top-2 right-2`}>
                             <span className={'max-sm:hidden'}>
+                        <div className={`badge ${novel.status} absolute top-2 right-2`}>
+                            <span>
                                 {novel.status === "Completed" && (
                                     <>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={5} stroke="currentColor" className="max-sm:size-3 size-5">
@@ -85,7 +87,7 @@ function LoadNovels({ novels }) {
                                 )}
                                 {novel.status === "OnGoing" && (
                                     <>
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="max-sm:size-3 size-5">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="max-sm:size-4 size-7">
                                             <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clipRule="evenodd" />
                                         </svg>
                                     </>
