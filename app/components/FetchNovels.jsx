28d25d821baf9b00e1a28496c519d49303794e3a
webspaghetti <ondrakohondra@email.cz>
2024-10-04 02:03:50 +0200

    Implement HoverRevealButton to homepage buttons

--- app/components/FetchNovels.jsx
+++ app/components/FetchNovels.jsx
@@ -2,6 +2,7 @@
 import LoadingPopup from "@/app/components/LoadingPopup";
 import { useState } from "react";
 import GetNovel from "@/app/components/functions/GetNovel";
+import HoverRevealButton from "@/app/components/HoverRevealButton";
 
 function FetchNovels() {
     const [buttonPopup, setButtonPopup] = useState(false);
@@ -95,12 +96,7 @@ function FetchNovels() {
 
     return (
         <>
-            <button onClick={handleButtonClick} className={"ml-2 svg-animate-rotate"} aria-label={"Refresh novels"}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
-                </svg>
-                <p className={'max-sm:hidden'}>Refresh novels</p>
-            </button>
+            <HoverRevealButton label={'Refresh novel'} onClick={handleButtonClick} animation={'svg-animate-rotate'} shape={'M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99'} />
             <LoadingPopup trigger={buttonPopup}/>
         </>
     );
