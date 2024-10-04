a927fa119688be8322507c6f50e75fa6cd7d5cdd
webspaghetti <ondrakohondra@email.cz>
2024-10-04 04:20:14 +0200

    Update NovelPage badges

--- app/[formatted_name]/NovelPage.jsx
+++ app/[formatted_name]/NovelPage.jsx
@@ -17,33 +17,33 @@ function NovelPage({novel, user}) {
                     <Image src={novel.alternative_image_url ? novel.alternative_image_url : novel.image_url} alt={`${novel.name} thumbnail`} width={350} height={350} quality={100} className={"select-none rounded-lg shadow-lg m-auto"} draggable="false" priority={true} />
                 </div>
                 <div className="flex flex-col justify-center items-start md:w-1/3 max-sm:w-full">
-                    <div className={`badge ${novel.status} max-sm:left-7 max-sm:p-3 max-sm:mt-2 mb-4 max-sm:absolute max-sm:top-0`}>
-                            <span className={'max-sm:hidden flex flex-row items-center'}>
-                                {novel.status === "Completed" && (
-                                    <>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={4} stroke="currentColor" className="max-sm:size-3 size-5">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-                                        </svg>
-                                        <span className="ml-1">Completed</span>
-                                    </>
-                                )}
-                                {novel.status === "OnGoing" && (
-                                    <>
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="max-sm:size-3 size-5">
-                                            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clipRule="evenodd" />
-                                        </svg>
-                                        <span className="ml-1">OnGoing</span>
-                                    </>
-                                )}
-                                {novel.status === "Hiatus" && (
-                                    <>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={4} stroke="currentColor" className="max-sm:size-3 size-5">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
-                                        </svg>
-                                        <span className="ml-1">Hiatus</span>
-                                    </>
-                                )}
-                            </span>
+                    <div className={`badge ${novel.status} max-sm:left-7 max-sm:p-1.5 max-sm:mt-2 mb-4 max-sm:absolute max-sm:top-0`}>
+                        <span className={'flex flex-row items-center'}>
+                            {novel.status === "Completed" && (
+                                <>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={5} stroke="currentColor" className="size-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                                    </svg>
+                                    <span className="ml-1 max-sm:hidden">Completed</span>
+                                </>
+                            )}
+                            {novel.status === "OnGoing" && (
+                                <>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="max-sm:size-7 size-5">
+                                        <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clipRule="evenodd" />
+                                    </svg>
+                                    <span className="ml-1 max-sm:hidden">OnGoing</span>
+                                </>
+                            )}
+                            {novel.status === "Hiatus" && (
+                                <>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={5} stroke="currentColor" className="size-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
+                                    </svg>
+                                    <span className="ml-1 max-sm:hidden">Hiatus</span>
+                                </>
+                            )}
+                        </span>
                     </div>
                     <h1 className="max-sm:text-2xl text-3xl font-bold mb-2">{novel.name}</h1>
                     <p className="max-sm:text-base text-lg mb-2">Chapters: {novel.chapter_count}</p>
