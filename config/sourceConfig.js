801ec355ee5bcb8850f284d43ba223b00398528c
webspaghetti <ondrakohondra@email.cz>
2025-10-13 17:40:25 +0200

    Add additional source for testing

--- config/sourceConfig.js
+++ config/sourceConfig.js
@@ -13,6 +13,19 @@ const sourceConfig = {
                 /[^\s<>]*?(?:[fƒḟ́])?[^\s<>]*?(?:r|г)[^\s<>]*?(?:e|е|ε|е́|ё|ē)[^\s<>]*?(?:e|е|ε|е́|ё|ē)[^\s<>]*?(?:w|ω|ѡ|ԝ)[^\s<>]*?(?:e|е|ε|е́|ё|ē)[^\s<>]*?[^\s<>]*?(?:[b|ɓ])[^\s<>]*?(?:n|ɳ|ո|ռ)[^\s<>]*?(?:o|о|ο|օ|σ)[^\s<>]*?(?:v|ν|ṿ|ṽ|ѵ)[^\s<>]*?(?:e|е|ε|е́|ё|ē)[^\s<>]*?l[^\s<>]*?(?:\.|\․|。|｡)?[^\s<>]*?(?:c|с|ϲ|ƈ)[^\s<>]*?(?:o|о|ο|օ|σ)[^\s<>]*?(?:m|м|ṃ|ṁ|๓)[^\s<>]*?(?=\s*<\/p>|\s*$)/gi,
             ]
         }
+    },
+    lightnovelworld: {
+        link_template: "https://www.lightnovelworld.co/novel/novel-name",
+        link_regex: /^https:\/\/www\.lightnovelworld\.co\/novel\/[a-z0-9-]+$/,
+        getInfoLink: (formattedName) => `https://www.lightnovelworld.co/novel/${formattedName}`,
+        getChapterLink: (params) => `https://www.lightnovelworld.co/novel/${params.formatted_name}/chapter-${params.chapter}`,
+        chapter_scraper: {
+            selectors: {
+                title: 'span.chapter-title',
+                content: 'div.chapter-content'
+            },
+            watermarkPatterns: []
+        }
     }
 }
 
