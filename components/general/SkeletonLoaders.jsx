8a66ce43edf57faa45c22a4cb7f363c66cca243a
webspaghetti <ondrakohondra@email.cz>
2025-10-13 19:42:35 +0200

    Update novel page skeleton

--- components/general/SkeletonLoaders.jsx
+++ components/general/SkeletonLoaders.jsx
@@ -2,20 +2,20 @@ function NovelPageSkeleton() {
     return (
         <div className="flex max-sm:flex-col flex-row max-sm:items-start justify-center items-center max-sm:gap-3 gap-8 border-b-[3px] border-gray-700 pb-4">
             <div className="w-1/2 max-sm:w-full animate-pulse">
-                <div className="bg-gray-700 rounded-lg h-[465px] w-[350px] m-auto" />
+                <div className="bg-gray-700 rounded-lg m-auto max-sm:h-[447px] max-sm:w-[335px] max-md:h-[401px] max-md:w-[301px] h-[467px] w-[350px]" />
             </div>
 
-            <div className="flex flex-col justify-center items-start md:w-1/3 max-sm:w-full gap-1">
-                <div className="flex flex-row items-center sm:gap-2 w-full">
-                    <div className="h-8 w-[90px] bg-gray-700 rounded-full animate-pulse mb-4" />
+            <div className="flex flex-col justify-center items-start md:w-1/3 max-sm:w-full gap-1 pt-0 max-sm:pt-1">
+                <div className="flex flex-row items-center sm:gap-2 w-full max-sm:hidden">
+                    <div className="h-[26px] w-[100px] bg-gray-700 rounded-full animate-pulse mb-4" />
                     <div className="h-6 w-6 bg-gray-700 rounded-full animate-pulse mb-4" />
                 </div>
 
-                <div className="h-9 w-3/4 bg-gray-700 rounded-lg animate-pulse mb-2" />
-                <div className="h-7 w-1/2 bg-gray-700 rounded-lg animate-pulse mb-2" />
-                <div className="h-5 w-2/3 bg-gray-700 rounded-lg animate-pulse" />
+                <div className="h-8 max-sm:h-7 w-3/4 bg-gray-700 rounded-lg animate-pulse mb-2" />
+                <div className="h-5 max-sm:h-5 max-sm:w-1/3 w-1/2 bg-gray-700 rounded-lg animate-pulse mb-2 gap-9" />
+                <div className="h-4 max-sm:h-3 max-sm:w-2/4 w-2/3 bg-gray-700 rounded-lg animate-pulse" />
 
-                <div className="h-12 w-52 bg-gray-700 rounded-lg animate-pulse my-2" />
+                <div className="h-12 max-sm:h-11 w-[195px] max-sm:w-[163px] bg-gray-700 rounded-lg animate-pulse my-2" />
 
                 <div className="w-full bg-gray-700 h-2 mt-1 rounded-xl">
                     <div className="bg-gray-600 h-2 rounded-xl w-0" />
