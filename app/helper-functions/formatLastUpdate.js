777318649c47b414f2e97ed443892074c6884543
webspaghetti <ondrakohondra@email.cz>
2025-10-13 16:13:59 +0200

    Add formatLastUpdate.js

---
+++ app/helper-functions/formatLastUpdate.js
@@ -0,0 +1,17 @@
+import wordsToNumbers from "words-to-numbers";
+
+function formatLastUpdate(lastUpdateText) {
+    // Remove 'Updated' and surrounding brackets
+    let formattedText = lastUpdateText.replace(/Updated |\[|]/g, '').trim();
+
+    // Split the text to isolate the time-related part
+    let timePart = formattedText.split(' ')[0];
+
+    // Check if the time part is a written number, and convert it if needed
+    const numericTimePart = isNaN(timePart) ? wordsToNumbers(timePart) : timePart;
+
+    // Rebuild the final string with the converted time part
+    return formattedText.replace(timePart, numericTimePart);
+}
+
+export default formatLastUpdate;
