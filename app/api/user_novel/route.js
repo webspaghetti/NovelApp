471e9887a5df193259b2b298aa29d2598985023d
webspaghetti <ondrakohondra@email.cz>
2025-10-14 16:20:37 +0200

    Rename user_progress into user_novel

--- app/api/user_novel/route.js
+++ app/api/user_novel/route.js
@@ -10,43 +10,45 @@ export async function GET(request) {
     try {
         let query, params;
         if (novelId) {
-            query = 'SELECT * FROM user_progress WHERE user_id = ? AND novel_id = ?';
+            query = 'SELECT * FROM user_novel WHERE user_id = ? AND novel_id = ?';
             params = [userId, novelId];
         } else {
-            query = 'SELECT * FROM user_progress WHERE user_id = ?';
+            query = 'SELECT * FROM user_novel WHERE user_id = ?';
             params = [userId];
         }
 
         const [rows] = await pool.query(query, params);
         return NextResponse.json(rows);
     } catch (error) {
-        console.error('Error fetching user progress:', error);
+        console.error('Error fetching user novel:', error);
         return NextResponse.json({ message: error.message }, { status: 500 });
     }
 }
+
 
 export async function POST(request) {
     try {
         const { userId, novelId } = await request.json();
 
         const [result] = await pool.query(
-            'INSERT INTO user_progress (user_id, novel_id, read_chapters) VALUES (?, ?, ?)',
+            'INSERT INTO user_novel (user_id, novel_id, read_chapters) VALUES (?, ?, ?)',
             [userId, novelId, '[]']
         );
 
         return NextResponse.json({ id: result.insertId, userId, novelId, read_chapters: '[]' }, { status: 201 });
     } catch (error) {
-        console.error('Error creating user progress:', error);
+        console.error('Error creating user novel:', error);
         return NextResponse.json({ message: error.message }, { status: 500 });
     }
 }
+
 
 export async function PUT(request) {
     try {
         const { userId, novelId, currentChapter } = await request.json();
 
         const [result] = await pool.query(
-            `UPDATE user_progress SET read_chapters = IF(JSON_CONTAINS(read_chapters, ?), read_chapters,
+            `UPDATE user_novel SET read_chapters = IF(JSON_CONTAINS(read_chapters, ?), read_chapters,
                                                          JSON_ARRAY_APPEND(read_chapters, '$', ?)), current_chapter = ? WHERE user_id = ? AND novel_id = ?`,
             [currentChapter, currentChapter, currentChapter, userId, novelId]
         );
@@ -55,7 +57,7 @@ export async function PUT(request) {
             return NextResponse.json({ message: "No matching record found" }, { status: 404 });
         }
 
-        return NextResponse.json({ message: "User progress updated successfully" }, { status: 200 });
+        return NextResponse.json({ message: "User novel updated successfully" }, { status: 200 });
     } catch (error) {
         console.error('Error updating user progress:', error);
         return NextResponse.json({ message: error.message }, { status: 500 });
