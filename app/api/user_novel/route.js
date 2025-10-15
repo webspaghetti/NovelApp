71144c9935339f026ea053f8d0c3f23e02c712e8
webspaghetti <ondrakohondra@email.cz>
2025-10-15 14:04:20 +0200

    Remove PUT route from user_novel api

--- app/api/user_novel/route.js
+++ app/api/user_novel/route.js
@@ -43,28 +43,6 @@ export async function POST(request) {
 }
 
 
-export async function PUT(request) {
-    try {
-        const { userId, novelId, currentChapter } = await request.json();
-
-        const [result] = await pool.query(
-            `UPDATE user_novel SET read_chapters = IF(JSON_CONTAINS(read_chapters, ?), read_chapters,
-                                                         JSON_ARRAY_APPEND(read_chapters, '$', ?)), current_chapter = ? WHERE user_id = ? AND novel_id = ?`,
-            [currentChapter, currentChapter, currentChapter, userId, novelId]
-        );
-
-        if (result.affectedRows === 0) {
-            return NextResponse.json({ message: "No matching record found" }, { status: 404 });
-        }
-
-        return NextResponse.json({ message: "User novel updated successfully" }, { status: 200 });
-    } catch (error) {
-        console.error('Error updating user progress:', error);
-        return NextResponse.json({ message: error.message }, { status: 500 });
-    }
-}
-
-
 export async function DELETE(request) {
     const { searchParams } = new URL(request.url);
     const userId = searchParams.get('userId');
