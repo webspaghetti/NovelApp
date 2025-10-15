500413728e50ed71cce337d656cb3789e9b27ef7
webspaghetti <ondrakohondra@email.cz>
2025-10-15 22:40:31 +0200

    Implement BackButton

--- app/[formatted_name]/[chapter]/page.jsx
+++ app/[formatted_name]/[chapter]/page.jsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import sourceConfig from "@/config/sourceConfig"
 import { notFound } from "next/navigation";
 import { updateUsersProgress } from "@/app/helper-functions/updateUsersProgress";
@@ -6,6 +5,7 @@ import { fetchNovelByFormattedName } from "@/app/helper-functions/fetchNovelByFo
 import ChapterDetails from "@/components/chapter-page/ChapterDetails";
 import ChapterNavigation from "@/components/chapter-page/ChapterNavigation";
 import ChapterStyleWrapper from "@/components/chapter-page/ChapterStyleWrapper";
+import BackButton from "@/components/novel-page/BackButton";
 
 
 async function fetchChapterContent(url) {
@@ -63,16 +63,7 @@ async function Page({ params }) {
             <main className={"px-5"}>
                 <div>
                     <div className="flex justify-start mb-5">
-                        <Link href={`/${formatted_name}`}>
-                            <button className={"group"}>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="max-sm:w-6 max-sm:h-6 w-7 h-7 sm:ml-1">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
-                                </svg>
-                                <span className="max-sm:hidden overflow-hidden opacity-0 max-w-0 group-hover:max-w-xs group-hover:opacity-100 transition-all duration-1000 ease-in-out">
-                                    <span className="pl-2 whitespace-nowrap">Back</span>
-                                </span>
-                            </button>
-                        </Link>
+                        <BackButton formattedName={formatted_name} />
                     </div>
 
                     <h1 className="border-b-gray-400 border-b-2 text-center max-sm:text-2xl text-3xl mb-4 pb-6 chapter-title">
