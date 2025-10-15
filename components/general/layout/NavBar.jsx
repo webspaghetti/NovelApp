99fe9540538ee3841b243f53b532fc025418615b
webspaghetti <ondrakohondra@email.cz>
2025-10-15 22:37:52 +0200

    Update NavBar.jsx to be server component to avoid content flicker

--- components/general/layout/NavBar.jsx
+++ components/general/layout/NavBar.jsx
@@ -1,21 +1,13 @@
-"use client"
 import Link from "next/link";
 import Image from "next/image";
-import { usePathname } from 'next/navigation'
-import { useState, useEffect } from 'react';
 
 
-function NavBar() {
-    const [isSpecificPage, setIsSpecificPage] = useState(false);
-    const pathName = usePathname();
+function NavBar({ pathname }) {
+    // Extract the last section of the pathname
+    const lastSection = pathname?.split('/').pop() || '';
 
-    useEffect(() => {
-        // Extract the last section of the pathname
-        const lastSection = pathName.split('/').pop();
-
-        // Check if the last section is a number (using isNaN)
-        setIsSpecificPage(!isNaN(parseInt(lastSection)));
-    }, [pathName]);
+    // Check if the last section is a number
+    const isSpecificPage = !isNaN(parseInt(lastSection)) && lastSection !== '';
 
     const navbarStyles = isSpecificPage ? "relative" : "fixed top-0 z-10";
 
@@ -25,16 +17,16 @@ function NavBar() {
             <nav>
                 <Link href={"/"}><div className={"flex items-center gap-5"}>
                     <Image src={"/logo.png"} alt={"NovelApp logo"} width={60} height={60} quality={100}/>
-                <h1 className={"max-sm:hidden text-2xl link_outline text-secondary"}>NovelApp</h1>
+                    <h1 className={"max-sm:hidden text-2xl link_outline text-secondary"}>NovelApp</h1>
                 </div></Link>
                 <div className={"flex items-center max-sm:gap-6 gap-14"}>
-                <Link href={"/"}><p className={'max-sm:hidden'}>Home</p></Link>
-                <Link href={"/about"}><p className={'max-sm:text-base'}>About</p></Link>
-                <Link href={"/profile"} aria-label={"Profile"}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-9 hover:ring-4 hover:ring-primary rounded-full transition-all">
-                        <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
-                    </svg>
-                </Link>
+                    <Link href={"/"}><p className={'max-sm:hidden'}>Home</p></Link>
+                    <Link href={"/about"}><p className={'max-sm:text-base'}>About</p></Link>
+                    <Link href={"/profile"} aria-label={"Profile"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-9 hover:ring-4 hover:ring-primary rounded-full transition-all">
+                            <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
+                        </svg>
+                    </Link>
                 </div>
             </nav>
         </div>
