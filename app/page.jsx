76e27552fd0e2583dd8aba79b821094f13b5b1d9
webspaghetti <ondrakohondra@email.cz>
2025-10-16 14:33:41 +0200

    Implement HomeClient

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,4 @@
-import NovelList from "@/components/homepage/NovelList";
-import AddNovelButton from "@/components/homepage/AddNovelButton";
-import SyncNovelsButton from "@/components/homepage/SyncNovelsButton";
-
+import HomeClient from "@/components/homepage/HomeClient";
 
 // Server-Side Render
 async function fetchNovels() {
@@ -26,28 +23,7 @@ async function Home() {
     const novelList = await fetchNovels();
     const userNovel = await fetchUserNovel();
 
-    const unObj = userNovel.reduce((acc, item) => {
-        acc[item.novel_id] = item;
-        return acc;
-    }, {});
-
-    return (
-        <main>
-            <div className={"flex justify-between w-full mb-5 relative top-[76px] max-sm:top-[70px]"}>
-                <AddNovelButton />
-                <SyncNovelsButton />
-            </div>
-
-            <div className="w-full h-[4px] bg-gradient-to-r from-primary via-secondary to-primary relative top-[76px] max-sm:top-[70px] mb-8 max-sm:mb-4 rounded-full" />
-
-            <div className={'max-md:flex max-md:justify-center'}>
-                <div className={"grid grid-cols-2 md:grid-cols-3 max-sm:gap-4 gap-5 md:gap-10 relative top-20 max-sm:pb-3 pb-9"}>
-                    <NovelList novelList={novelList} initialUserNovel={unObj} />
-                </div>
-            </div>
-        </main>
-    );
+    return <HomeClient novelList={novelList} userNovel={userNovel} />;
 }
-
 
 export default Home;
