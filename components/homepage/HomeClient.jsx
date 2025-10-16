ad7ee1f1900b0cf995f8f32ab6c5fd66a17ea292
webspaghetti <ondrakohondra@email.cz>
2025-10-16 22:21:17 +0200

    Implement passing SyncNovelsButton novelList prop

--- components/homepage/HomeClient.jsx
+++ components/homepage/HomeClient.jsx
@@ -104,7 +104,7 @@ function HomeClient({ novelList, userNovel }) {
             <div className={"flex justify-between w-full mb-5 relative top-[76px] max-sm:top-[70px]"}>
                 <AddNovelButton />
                 <AnimatedIconButton label={'Search filter'} isActive={sfVisible} onClick={() => setSfVisible(!sfVisible)} animation={'svg-animate-scale'} shape={'M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75'} />
-                <SyncNovelsButton />
+                <SyncNovelsButton novelList={novelList}/>
             </div>
             <div hidden={!sfVisible}>
                 <NovelSearchFilter
