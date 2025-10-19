ef963982fcdacbab2250694d6392e16eeb09a934
webspaghetti <ondrakohondra@email.cz>
2025-10-19 16:07:45 +0200

    Improve the handling of faulty chapters

--- components/chapter-page/ChapterDetails.jsx
+++ components/chapter-page/ChapterDetails.jsx
@@ -1,3 +1,4 @@
+"use client"
 function ChapterDetails({ chapter }) {
     return (
         <div className="max-sm:text-base text-lg pb-4 border-b-gray-400 border-b-2 chapter-content">
@@ -8,10 +9,16 @@ function ChapterDetails({ chapter }) {
                     }}
                 />
             ) : (
-                <div className={'text-center text-xl'}>
-                    <p>This is most likely caused by the novel using partial chapter structure (1.1, 1.2, etc.).</p>
-                    <p>Unfortunately, right now this makes these novels unavailable.</p>
-                    <p>We&apos;re sorry :(</p>
+                <div className="flex flex-col justify-center items-center text-center text-xl">
+                    <p>Failed to display the chapter.</p>
+                    <p>Retry, and if the issue continues across multiple chapters, consider changing the novel&apos;s source.</p>
+                    <button className={"mt-4"} onClick={() => location.reload()}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2z"/>
+                            <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466"/>
+                        </svg>
+                        Retry
+                    </button>
                 </div>
             )}
         </div>
