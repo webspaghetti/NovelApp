e9a5a9ae38944747caf75d3629a4a21a209ab32b
webspaghetti <ondrakohondra@email.cz>
2025-10-19 16:58:21 +0200

    Update formatting

--- components/homepage/NovelSearchFilter.jsx
+++ components/homepage/NovelSearchFilter.jsx
@@ -7,16 +7,16 @@ function NovelSearchFilter({onSearchChange, onStatusFilter, onSortChange, curren
     const [showFilters, setShowFilters] = useState(false);
 
 
-    const handleSearchChange = (e) => {
+    function handleSearchChange(e) {
         const value = e.target.value;
         setSearchTerm(value);
         onSearchChange(value);
-    };
+    }
 
-    const clearSearch = () => {
+    function clearSearch() {
         setSearchTerm("");
         onSearchChange("");
-    };
+    }
 
 
     return (
@@ -25,20 +25,20 @@ function NovelSearchFilter({onSearchChange, onStatusFilter, onSortChange, curren
             <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-3">
                 {/* Search Bar */}
                 <div className="relative flex-1 border border-gray-600 bg-navbar rounded-lg px-2 py-2 focus-within:border-primary transition-all">
-          <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="size-5 sm:size-6"
-            >
-              <path
-                  fillRule="evenodd"
-                  d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
-                  clipRule="evenodd"
-              />
-            </svg>
-          </span>
+                    <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
+                      <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="size-5 sm:size-6"
+                      >
+                        <path
+                            fillRule="evenodd"
+                            d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+                            clipRule="evenodd"
+                        />
+                      </svg>
+                    </span>
                     <input
                         type="text"
                         placeholder="Search novels by name"
