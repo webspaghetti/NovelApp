ce6f2fddbd1210660ef060e00b333726d1f96798
webspaghetti <ondrakohondra@email.cz>
2025-10-21 21:34:44 +0200

    Move and update fetchNovelByFormattedNameAndSource.js

--- app/api/novels/util/fetchNovelByFormattedNameAndSource.js
+++ app/api/novels/util/fetchNovelByFormattedNameAndSource.js
@@ -1,10 +1,12 @@
 export async function fetchNovelByFormattedNameAndSource(formatted_name, source) {
     try {
-        const apiUrl = process.env.PUBLIC_API_URL || '';
-        const response = await fetch(`${apiUrl}/api/novels?formattedName=${encodeURIComponent(formatted_name)}&source=${encodeURIComponent(source)}`);
+        const response = await fetch(`/api/novels?formattedName=${encodeURIComponent(formatted_name)}&source=${encodeURIComponent(source)}`, {
+
+        });
         if (!response.ok) {
             throw new Error('Failed to fetch novel');
         }
+
         const data = await response.json();
         return data[0];
 
