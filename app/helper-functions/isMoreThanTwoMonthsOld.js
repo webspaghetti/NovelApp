781d7efe5a7e93c7907b412f4d56a799b6d2defa
webspaghetti <ondrakohondra@email.cz>
2025-10-22 16:25:14 +0200

    Update isMoreThanTwoMonthsOld.js logic for regular date

--- app/helper-functions/isMoreThanTwoMonthsOld.js
+++ app/helper-functions/isMoreThanTwoMonthsOld.js
@@ -5,7 +5,13 @@ export function isMoreThanTwoMonthsOld(update) {
     } else if (update.includes('a year ago') || update.includes('years ago')) {
         return true;
     } else if (Date.parse(update)) {
-        return true;
+        const currentDate = new Date();
+        const parsedDate = new Date(update);
+
+        // Calculate the difference in months between the current date and the parsed date
+        const diffInMonths = (currentDate.getFullYear() - parsedDate.getFullYear()) * 12 + currentDate.getMonth() - parsedDate.getMonth();
+
+        return diffInMonths >= 3;
     }
     return false;
 }
