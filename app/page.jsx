48a3f16878bf6b7ced55acbb45404fbedc03204b
webspaghetti <ondrakohondra@email.cz>
2025-11-08 18:20:45 +0100

    Update homepage with new methods

--- app/page.jsx
+++ app/page.jsx
@@ -1,58 +1,24 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth/next";
-import HomeClient from "@/components/homepage/HomeClient";
+import { fetchAllNovels, fetchAllUserNovels } from "@/lib/commonQueries";
 import NavBar from "@/components/general/layout/NavBar";
-import pool from "@/lib/db";
+import HomeClient from "@/components/homepage/HomeClient";
 
-async function fetchNovels(userId) {
-    try {
-        let query, params;
-
-        query = `
-                SELECT n.*
-                FROM novel_table n
-                JOIN user_novel un ON n.id = un.novel_id
-                WHERE un.user_id = ?
-            `;
-        params = [userId];
-
-        const [rows] = await pool.query(query, params);
-
-        return rows;
-    } catch (error) {
-        console.error('Error fetching novels:', error);
-        return [];
-    }
-}
-
-async function fetchUserNovel(userId) {
-    try {
-        let query, params;
-
-        query = 'SELECT * FROM user_novel WHERE user_id = ?';
-        params = [userId];
-
-        const [rows] = await pool.query(query, params);
-
-        return rows;
-    } catch (error) {
-        console.error('Error fetching user novel:', error);
-        return null;
-    }
-}
 
 async function Home() {
     const session = await getServerSession(authOptions);
 
-    const novelList = await fetchNovels(session.user.id);
-    const userNovel = await fetchUserNovel(session.user.id);
+    const novelList = await fetchAllNovels(session.user.id);
+    const userNovels = await fetchAllUserNovels(session.user.id);
+
 
     return (
         <>
             <NavBar />
-            <HomeClient novelList={novelList} userNovel={userNovel} session={session} />;
+            <HomeClient novelList={novelList} userNovel={userNovels} session={session} />;
         </>
     );
 }
+
 
 export default Home;
