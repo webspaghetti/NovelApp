9ae7a526c8803287cc720c1c0012243d7e0ab4e2
webspaghetti <ondrakohondra@email.cz>
2025-11-09 15:21:32 +0100

    Update formatting

--- components/templates/tabs/reader/NavigationTab.jsx
+++ components/templates/tabs/reader/NavigationTab.jsx
@@ -38,8 +38,7 @@ function NavigationTab({ settings, setSettings, handleRestore, editMode }) {
                         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-4">
                             {/* Nav Background Color */}
                             <div>
-                                <label className="block text-sm font-semibold text-secondary mb-2 select-none">Background
-                                    Color</label>
+                                <label className="block text-sm font-semibold text-secondary mb-2 select-none">Background Color</label>
                                 <div className="flex gap-2 w-full">
                                     <input
                                         type="color"
