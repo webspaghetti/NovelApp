5572dd5cc8256165e6cb69d1de6e96a15b31c0bf
webspaghetti <ondrakohondra@email.cz>
2025-11-11 21:44:35 +0100

    Update dateFormatterFull return string

--- app/helper-functions/dateFormatterFull.js
+++ app/helper-functions/dateFormatterFull.js
@@ -9,5 +9,5 @@ export function dateFormatterFull(dateString) {
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
 
-    return `${day}.${month}.${year}, ${hours}:${minutes}:${seconds}`;
+    return `${day}.${month}.${year} | ${hours}:${minutes}:${seconds}`;
 }
